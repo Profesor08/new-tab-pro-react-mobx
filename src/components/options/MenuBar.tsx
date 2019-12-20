17ba9ae5109103803bd2b96bfe7172d8ee22015a
Profesor08 <online7890@gmail.com>
2019-12-20 16:18:03 +0200

    updated menu

--- src/components/options/MenuBar.tsx
+++ src/components/options/MenuBar.tsx
@@ -1,69 +1,76 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Dispatch } from "redux";
-import style from "./options.module.scss";
-import MenuBarButton from "./MenuBarButton";
-import {
-  toggleOptionsPanel,
-  OptionsAppInitialState,
-} from "../../store/reducers/optionsApp";
-import { toggleBookmarksPanel } from "../../store/reducers/bookmarksApp";
+import React from "react";
+import { observer } from "mobx-react";
+import options from "../../store-mobx/options";
+import styled from "styled-components";
+import OptionsPanel from "./OptionsPanel";
 
-interface MenuBarStateProps {
-  optionsPanelShow: boolean;
-}
+const Icon = styled.i`
+  font-style: normal;
+  transition: ease color 0.2s;
+`;
 
-interface MenuBarDispatchProps {
-  openOptionsPanel: () => void;
-  openBookmarksPanel: () => void;
-}
+const MenuButton = styled.div.attrs({
+  tabIndex: -1,
+})`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 100%;
+  width: var(--options-button-size);
+  height: var(--options-button-size);
+  background-color: var(--options-background-color);
+  border: 2px solid var(--options-border-color);
+  border-radius: var(--options-border-radius);
+  color: var(--options-button-color);
+  transition: ease all 0.2s;
+  outline: none;
 
-interface MenuBarState {
-  optionsApp: OptionsAppInitialState;
-}
+  &:hover,
+  &:focus,
+  &:focus-within {
+    cursor: pointer;
+    width: var(--options-hover-button-size);
+    height: var(--options-hover-button-size);
+    border-radius: var(--options-hover-border-radius);
+    border-color: var(--options-hover-border-color);
+    background-color: var(--options-hover-background-color);
 
-type Props = MenuBarStateProps & MenuBarDispatchProps;
-
-class MenuBar extends Component<Props> {
-  render() {
-    return (
-      <div className={style["options-bar"]}>
-        <MenuBarButton
-          onClick={this.props.openOptionsPanel}
-          iconStyle={{ transform: "scale(1.3, 1)" }}
-        >
-          ☰
-        </MenuBarButton>
-        <MenuBarButton
-          onClick={this.props.openBookmarksPanel}
-          iconStyle={{ fontSize: "1.37rem" }}
-        >
-          ★
-        </MenuBarButton>
-      </div>
-    );
+    ${Icon} {
+      color: var(--options-hover-button-color);
+    }
   }
-}
+`;
 
-const mapStateToProps = (state: MenuBarState): MenuBarStateProps => {
-  return {
-    optionsPanelShow: state.optionsApp.optionsPanelShow,
-  };
-};
+const MenuBar = styled.div`
+  position: fixed;
+  top: 10px;
+  left: 10px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 10px;
+  user-select: none;
+  z-index: 100;
+`;
 
-const mapDispatchProps = (dispatch: Dispatch) => {
-  return {
-    openOptionsPanel: () => {
-      dispatch(toggleOptionsPanel(true));
-    },
+export const Menu = observer(() => {
+  return (
+    <MenuBar>
+      <MenuButton
+        onFocus={() => {
+          options.optionsPanelShow = true;
+        }}
+        onBlur={() => {
+          options.optionsPanelShow = false;
+        }}
+      >
+        <Icon>☰</Icon>
+        <OptionsPanel />
+      </MenuButton>
+      <MenuButton onFocus={() => {}}>
+        <Icon>★</Icon>
+      </MenuButton>
+    </MenuBar>
+  );
+});
 
-    openBookmarksPanel: () => {
-      dispatch(toggleBookmarksPanel(true));
-    },
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchProps,
-)(MenuBar);
+export default Menu;
