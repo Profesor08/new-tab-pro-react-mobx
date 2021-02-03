45747bec8496cbe9e0751d0d21c7e69eb91f2826
Profesor08 <online7890@gmail.com>
2021-02-03 23:29:54 +0200

    docs folder for gh

---
+++ docs/precache-manifest.b33c5cd7e74528678b9d56c6c80081f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "bf9a37ecfc6369fca1bb",
+    "url": "/static/js/main.bf9a37ec.chunk.js"
+  },
+  {
+    "revision": "c586d7da49caade7a9e5",
+    "url": "/static/js/2.c586d7da.chunk.js"
+  },
+  {
+    "revision": "5397dbafac3332350d8615731450a4f3",
+    "url": "/index.html"
+  }
+];
