d42564f47f023595d7dcfbf26f517614c6906d4b
Oleg Baltag <online7890@gmail.com>
2023-01-06 23:41:56 +0200

    fix animation dupes

--- src/components/weather-widget/Icon.tsx
+++ src/components/weather-widget/Icon.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import lottie from "lottie-web";
 import styled from "styled-components";
 import { getWeatherIcon } from "./utils";
@@ -6,32 +6,30 @@ import { getWeatherIcon } from "./utils";
 const Icon = styled.div``;
 
 export const WeatherIcon = ({ type }: { type: string }) => {
-  const ref: React.MutableRefObject<null | HTMLDivElement> = useRef(null);
-
-  let animation: lottie.AnimationItem | null = null;
+  const ref = useRef<HTMLDivElement>(null);
+  const [animation, setAnimation] = useState<lottie.AnimationItem>();
 
   useEffect(() => {
-    animation = lottie.loadAnimation({
-      wrapper: ref.current,
-      loop: false,
-      autoplay: true,
-      renderer: "svg",
-      animationData: getWeatherIcon(type),
-      rendererSettings: {
-        LM: true,
-        className: "yXJMw",
-      },
-    });
-  });
+    setAnimation((animation) => {
+      animation?.destroy();
 
-  return (
-    <Icon
-      ref={ref}
-      onMouseEnter={() => {
-        if (animation) {
-          animation.playSegments([0, animation.totalFrames], true);
-        }
-      }}
-    />
-  );
+      return lottie.loadAnimation({
+        wrapper: ref.current,
+        loop: false,
+        autoplay: true,
+        renderer: "svg",
+        animationData: getWeatherIcon(type),
+        rendererSettings: {
+          LM: true,
+          className: "yXJMw",
+        },
+      });
+    });
+  }, [type]);
+
+  const replay = useCallback(() => {
+    animation?.playSegments([0, animation.totalFrames], true);
+  }, [animation]);
+
+  return <Icon ref={ref} onPointerEnter={replay} />;
 };
